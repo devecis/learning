aa7efc0a61e7dfb5d3c06dacef29139272fe50d6
Sea Ndeveci <dev@localhost.localdomain>
2020-04-07 14:24:14 -0400

    Created week3_easy

--- Week 3/Week3_easy.py
+++ Week 3/Week3_easy.py
@@ -1,31 +1,14 @@
 #Write a function that flips a coin. It should return a string that reads 'Heads' or 'Tails'. Bonus points if you add the ability to make the coin unfair.
 import random
 
-coin = input("How many times would you like to flip the coin or Q to quit: ")
-head=0
-tail=1
+coin = int(input("How many times would you like to flip the coin: "))
 detail=[]
-for times in range(coin):
-    flip=random.randint(0,1)
+flip=random.randint(0,1)
+for i in range(coin):
     if (flip == 0):
-        detail.append('heads')
+        detail.append('Heads')
     else:
         detail.append('Tails')
 print(detail)
-    
-
-#import random
-#def coinToss(number):
-#    number = input
-#    recordList = [0,1]
-#    heads = 0
-#    tails = 1
-#    flip = random.random()
-#    if (flip == 0):
-#        print("Heads")
-#        recordList.append("Heads")
-#    else:
-#        print("Tails")
-#        recordList.append("Tails")
-#    print(str(recordList))
-#    print(str(recordList.count("Heads")) + str(recordList.count("Tails")))
+#print(str('You got heads'),detail.count('Heads')) + str('You got tails '),detail.count('Tails')
+print(str('You got Heads'), detail.count('Heads'), str('times and you got tails '), detail.count('Tails'), str('times.'))
