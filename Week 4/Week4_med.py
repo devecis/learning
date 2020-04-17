975e7deef07aa948fa45ce4e759d8b61ad663a23
Sea Ndeveci <dev@localhost.localdomain>
2020-04-16 22:30:40 -0400

    Weekly Medium Challenge

--- Week 4/Week4_med.py
+++ Week 4/Week4_med.py
@@ -1,3 +1,27 @@
 # Write a class!
-# I want a 'vehicle' class, that has a data structure for seats (Maybe a list of lists? A dict of lists?), and attributes like weight, fuel capacity. Write functions to allow the setting and getting of these attributes. Bonus points if you sort out a way to store passengers in seats.
+# I want a 'vehicle' class, that has a data structure for
+# seats (Maybe a list of lists? A dict of lists?), and attributes like
+# weight,
+#  fuel capacity. Write functions to allow the setting and getting of these attributes. Bonus points if you sort out a way to store passengers in seats.
 # See here: https://docs.python.org/3/tutorial/classes.html
+
+
+class Vehicles:
+    """ For defining vehicles"""
+
+    def __init__(self, make, seat_rows, seat_columns, weight, fuel_capacity):
+        self.type = make
+        self.row = seat_rows
+        self.column = seat_columns
+        self.weight = weight
+        self.fuel_cap = fuel_capacity
+                   
+        self.grid = {}
+        for rows in range(seat_rows):
+            seats = []
+            for cols in range(seat_columns):
+                seats.append(None)
+                self.grid.update({rows: seats})
+
+C130 = Vehicles('lockheed', 40, 2, 4324323424, 40000)
+print(C130.grid)
