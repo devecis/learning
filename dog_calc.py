d5196df095ef1ab9ff9cb45a7856ca4ac50299b7
dev01 <nerdhub@gmail.com>
2021-03-18 13:36:23 -0400

    changes

--- dog_calc.py
+++ dog_calc.py
@@ -5,6 +5,6 @@
 
 name = input("what is your dogs name? ")
 age =  int(input("How old is your dog? "))
-# human = age*7
 # print(f'Your dog, {name}, is {human} years old in human years.')
 print(f"You dog {name}, is", age * 7, "in human years")
+print(f"{name} can't spell your, because he sucks")
