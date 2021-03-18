e48a09beae5c20ba16310fa55eadbc9e68d7dc1b
dev01 <nerdhub@gmail.com>
2021-03-18 16:40:44 -0400

    cahnged 15 to 7

--- Week 5/week5_easy2.py
+++ Week 5/week5_easy2.py
@@ -1,14 +1,15 @@
 # Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
 
-num = []
-for i in range(101):
+#num = []
+for i in range(1, 101):
     if i % 3 == 0:
         print("fizz")
         continue
     elif i % 5 == 0:
         print("buzz")
         continue
-    elif i % 15 == 0:
+    elif i % 7 == 0:
         print("Fizz Buzz")
         continue
     print(i)
+    
