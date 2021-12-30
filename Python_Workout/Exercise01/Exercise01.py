d4f8ae787eff11dcdb41a2ab4f2202dfa4d4de1c
iubuw7hw <iubuw7hw@mail.gitlabdemo.cloud>
2021-12-29 22:22:42 -0500

    workouts

---
+++ Python_Workout/Exercise01/Exercise01.py
@@ -0,0 +1,31 @@
+"""
+Write a function (guessing_game) that takes no arguments.
+When run, the function chooses a random interger between 0 - 100 inclusive
+Then asks the user to guess what number has been chosen
+Each time the user enters a guess, the program indicates one of the following:
+- Too High
+- Too Low
+- Just right
+If the user guesses correctly, the program exits. Otherwises , the user is asked to try agian
+The program exits after the user guess correctly
+Give user 3 tries
+"""
+
+
+import random
+
+def guessing_game():
+    answer = random.randint(1, 100)
+
+    for i in range(3):
+        user_guess = int(input("Guess a number between 1 - 100, you only get 3 tries. "))
+        if user_guess == answer:
+            print(f"Right! The answer is {user_guess}")
+            break
+        if user_guess < answer:
+               print(f"Your guess of {user_guess} is too low!")         
+        else:
+            if user_guess > answer:
+                print(f"Your guess of {user_guess} is too high!")
+              
+guessing_game()
